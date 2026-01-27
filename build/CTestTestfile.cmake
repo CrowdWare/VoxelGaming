2add2f3c1e131f179ae19311caf582dcd73d9450
Artanidos <japp.olaf@gmail.com>
2026-01-27 22:26:09 +0100

    rotation now working

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/art/SourceCode/VoxelGaming
+# Build directory: /Users/art/SourceCode/VoxelGaming/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("SMLParser")
+subdirs("SMLUI")
+subdirs("VoxelEngine")
+subdirs("RaidBuilder")
