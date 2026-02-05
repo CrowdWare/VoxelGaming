30232e03e2b9ae040bc6ae97a0e5fd8e63e9ff23
Artanidos <japp.olaf@gmail.com>
2026-02-05 10:58:23 +0100

    mesh cache added

--- run_builder.sh
+++ run_builder.sh
@@ -2,5 +2,8 @@
 set -e
 
 export DYLD_LIBRARY_PATH="$VULKAN_SDK/lib:${DYLD_LIBRARY_PATH:-}"
-
+export DEBUG_MESHES=1
+export DEBUG_MESH_LOAD=1
+export DEBUG_DUNGEON_LOAD=1
+export DEBUG_MESH_CACHE=1
 ./build/RaidBuilder/RaidBuilder
